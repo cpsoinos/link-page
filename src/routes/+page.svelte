d3b797eac98415a8d06b5f651ca20c9c3f72026a
Corey Psoinos <coreypsoinos@gmail.com>
2025-02-12 08:15:36 -0500

    add meta tags and favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,27 @@
 	import Contacts from '$lib/components/contacts.svelte';
 </script>
 
+<svelte:head>
+	<title>Production Department @ The Public Theater</title>
+	<meta
+		name="description"
+		content="Contact information for the Production Department at The Public Theater."
+	/>
+	<meta
+		name="keywords"
+		content="The Public Theater, Production Department, Scenery, Props, Lighting, Audio, Costumes, Video, Production Management"
+	/>
+	<meta name="author" content="The Public Theater" />
+	<meta name="robots" content="index, follow" />
+
+	<meta property="og:title" content="Production Department @ The Public Theater" />
+	<meta
+		property="og:description"
+		content="Contact information for the Production Department at The Public Theater."
+	/>
+	<meta property="og:type" content="website" />
+</svelte:head>
+
 <main>
 	<Header />
 	<Contacts />
